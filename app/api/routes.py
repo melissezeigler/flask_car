98912094829c8edf2b2c14af67229c4a759e1e2b
Melisse Zeigler <melissebernadas@gmail.com>
2023-05-29 19:30:44 -0800

    car project render update

---
+++ app/api/routes.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Car, car_schema, cars_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'yee': 'HHHAAAWWWWWWW'}
+
+@api.route('/data')
+def viewdata():
+    data = get_car()
+    response = jsonify(data)
+    print(response)
+    return render_template('index.html', data = data)
+
+#Create car entry
+@api.route('/cars', methods = ['POST'])
+@token_required
+def create_car(current_user_token):
+    car_make = request.json['car_make']
+    car_model = request.json['car_model']
+    car_year = request.json['car_year']
+    car_mileage = request.json['car_mileage']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    car = Car(car_make, car_model, car_year, car_mileage, user_token = user_token )
+
+    db.session.add(car)
+    db.session.commit()
+
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+# GET ALL cars
+@api.route('/cars', methods = ['GET'])
+@token_required
+def get_all_cars(current_user_token):
+    a_user = current_user_token.token
+    cars = Car.query.filter_by(user_token = a_user).all()
+    response = cars_schema.dump(cars)
+    return jsonify(response)
+
+# GET SINGLE car
+@api.route('/cars/<id>', methods = ['GET'])
+@token_required
+def get_single_car(current_user_token, id):
+    car = Car.query.get(id)
+    response = car_schema.dump(car)
+    return jsonify(response)
+   
+
+# UPDATE car
+@api.route('/cars/<id>', methods = ['POST','PUT'])
+@token_required
+def update_car(current_user_token,id):
+    car = Car.query.get(id) 
+    car.name = request.json['car_make']
+    car.email = request.json['car_model']
+    car.phone_number = request.json['car_year']
+    car.address = request.json['car_mileage']
+    car.user_token = current_user_token.token
+
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+
+# DELETE car 
+@api.route('/cars/<id>', methods = ['DELETE'])
+@token_required
+def delete_car(current_user_token, id):
+    car = Car.query.get(id)
+    db.session.delete(car)
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+
+
+
+
